3c2f2683a365458f5a858bd51b002fa96058cc8d
Maximus <maximus.slabon@gmail.com>
2023-09-17 16:19:56 -0400

    hotfix treesitter config

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,12 +3,12 @@ if not status_ok then
     return
 end
 
-require("nvim-treesitter.configs").setup {
+treesitter.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "c_sharp" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
@@ -16,10 +16,6 @@ require("nvim-treesitter.configs").setup {
 
   highlight = {
     enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
 }
